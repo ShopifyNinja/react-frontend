9eb0e250c2363da2df3a2b82081d907a46209e4f
ShopifyNinja <shopify.ninja9296@gmail.com>
2021-03-24 00:43:37 +0900

    first commit

---
+++ src/api/types.ts
@@ -0,0 +1,43 @@
+declare global {
+    interface Config {
+        api: {
+            authUrl: string;
+            tradeUrl: string;
+            applogicUrl: string;
+            rangerUrl: string;
+            finexUrl: string;
+        };
+        finex: string | boolean;
+        withCredentials: string | boolean;
+        incrementalOrderBook: string | boolean;
+        isResizable: string | boolean;
+        isDraggable: string | boolean;
+        showLanding: string | boolean;
+        sentryEnabled: string | boolean;
+        captchaLogin: string | boolean;
+        usernameEnabled: string | boolean;
+        gaTrackerKey: string;
+        minutesUntilAutoLogout: string;
+        msAlertDisplayTime: string;
+        sessionCheckInterval: string;
+        balancesFetchInterval: string;
+        passwordEntropyStep: string | number;
+        storage: {
+            defaultStorageLimit: string | number;
+            orderBookSideLimit: string | number;
+        };
+        languages: string[];
+        kycSteps: string[];
+        captcha_type: 'recaptcha' | 'geetest' | 'none';
+        captcha_id?: string;
+        password_min_entropy: string | number;
+        palette?: string;
+        wizard_step?: string;
+    }
+
+    interface Window {
+        env: Config;
+    }
+}
+
+export {};
---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,92 @@
+import classnames from 'classnames';
+import * as React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { ChevronIcon } from '../../assets/images/ChevronIcon';
+import { convertToString } from '../../helpers';
+
+type DropdownElem = number | string | React.ReactNode;
+
+export interface DropdownComponentProps {
+    /**
+     * List of options
+     */
+    list: DropdownElem[];
+    /**
+     * Selection callback function
+     * @default empty
+     */
+    onSelect?: (index: number) => void;
+    /**
+     *  By default class name 'cr-dropwdown'
+     *  This property gives an additional class name
+     *  @default empty
+     */
+    className?: string;
+    /**
+     * Value for placeholder of Dropdown components
+     * @default empty
+     */
+    placeholder?: string;
+    /**
+     * Value for disabling contentEditable property
+     * @default false
+     */
+    disableContentEditable?: boolean;
+    /**
+     * Value for clear selected item
+     * @default false
+     */
+    clear?: boolean;
+}
+
+
+/**
+ *  Cryptobase Dropdown that overrides default dropdown with list of options.
+ */
+
+export const DropdownComponent = (props: DropdownComponentProps) => {
+    const [selected, setSelected] = React.useState<string | undefined>('');
+
+    const { list, className, placeholder, clear, onSelect } = props;
+    const defaultPlaceholder = list[0];
+
+    const cx = React.useMemo(() => classnames('cr-dropdown', className, {
+        'cr-dropdown--default': selected === placeholder,
+    }), [selected, placeholder, className]);
+
+    const handleSelect = React.useCallback((elem: DropdownElem, index: number) => {
+        onSelect && onSelect(index);
+        setSelected(convertToString(elem));
+    }, [onSelect]);
+
+    const renderElem = React.useCallback((elem: DropdownElem, index: number) => {
+        return  (
+            <Dropdown.Item
+                key={index}
+                onSelect={() => handleSelect(elem, index)}
+            >
+                {elem}
+            </Dropdown.Item>
+        );
+    }, [handleSelect]);
+
+    React.useEffect(() => {
+        if (clear !== false) {
+            setSelected(placeholder || convertToString(defaultPlaceholder));
+        }
+    }, [placeholder, defaultPlaceholder, clear]);
+
+    return (
+        <div className={cx}>
+            <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                    {selected}
+                    <ChevronIcon className="cr-dropdown__arrow" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {list.map(renderElem)}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
+};
